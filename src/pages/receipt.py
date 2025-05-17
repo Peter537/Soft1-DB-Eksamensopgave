3a9e6adfc15c4902b2fe5681f5d4e1d1f38a5339
Ossi <113044607+cph-oo221@users.noreply.github.com>
2025-05-17 16:39:06 +0200

    Update receipt.py

--- src/pages/receipt.py
+++ src/pages/receipt.py
@@ -2,12 +2,31 @@ import streamlit as st
 
 def render():
     st.title("Receipt")
-    st.write("placeholder for receipt")
+
+    title, price, quantity, seller_id = st.columns(4)
+    with title:
+        st.write("**Title**")
+    with price:
+        st.write("**Price**")
+    with quantity:
+        st.write("**Quantity**")
+    with seller_id:
+        st.write("**Seller ID**")
+
+    total_amount = 0    
 
     for item in st.session_state.bought_items:
-        st.write(item["posting_id"])
-        st.write(item['title'])
-        st.write(item['price'])
-        st.write(item['quantity'])
+        col1, col2, col3, col4 = st.columns(4)
+        with col1:
+            st.write(item["title"])
+        with col2:
+            st.write(f"${item['price']}")
+            total_amount += item["price"]
+        with col3:
+            st.write(item["quantity"])
+        with col4:
+            st.write(item["seller_id"])
         st.write("---")
     
+    st.write(f"**Total Amount: ${total_amount}**")
+    st.write("Thank you for your purchase!")
